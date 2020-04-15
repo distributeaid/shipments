ec066a163cc9f6d4ce3d73856390080cf030a9e9
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-04-15 09:18:26 +0200

    fix: enable production mode for CI builds

--- scripts/renderIndex.js
+++ scripts/renderIndex.js
@@ -6,7 +6,7 @@ const Handlebars = require('handlebars')
 console.log(
 	html({
 		VERSION: getVersion(),
-		IS_PRODUCTION: JSON.stringify(false),
+		IS_PRODUCTION: JSON.stringify(true),
 		SITE_DIR: process.env.SITE_DIR,
 		SHIPMENTS_URL: new Handlebars.SafeString(process.env.SHIPMENTS_URL),
 		FALLBACK_SHIPMENTS: new Handlebars.SafeString(
